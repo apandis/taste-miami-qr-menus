8995188c009ff97ab80fa360893fd35ff26ab4a9
Antonios Pandis <pandis.a@gmail.com>
2022-03-14 23:28:19 -0400

    add breakfast

--- src/views/EpicureView.vue
+++ src/views/EpicureView.vue
@@ -4,9 +4,29 @@
       <v-col cols="12">
         <h2 class="text-h4">Epicure</h2>
       </v-col>
-      <v-col cols="12 d-flex justify-space-between">
-        <v-chip label>Breakfast</v-chip><v-chip label>Lunch</v-chip
-        ><v-chip label>Dinner</v-chip>
+      <v-col cols="12">
+        <v-chip class='mr-2' label>Breakfast</v-chip>
+        <v-chip class='mr-2' label>Lunch</v-chip>
+        <v-chip class='mr-2' label>Dinner</v-chip>
+        <v-chip class='mr-2' label>Specials</v-chip>
+        <v-chip class='mr-2' label>Beverages</v-chip>
+        <v-chip class='mr-2' label>Bar Bites</v-chip>
+      </v-col>
+      <v-col cols="12">
+        <h5 class="text-h5">Breakfast</h5>
+        <h6 class="text-h6">Breakfast your way</h6>
+        <p class="text-body-1">includes selection of: eggs your way, chicken or pork sausage, bacon, potatoes, toast, artisan pastries, served with orange juice and coffee.</p>
+        <h6 class="text-h6">Omelette</h6>
+        <p class="text-body-1">choose three: onions, bell peppers, mushrooms, tomatoes, spinach, mozzarella, cheddar cheese, kale, brussels sprouts, ham, toast.</p>
+      </v-col>
+      <v-col cols="12">
+        <h5 class="text-h5">Lunch</h5>
+      </v-col>
+      <v-col cols="12">
+        <h5 class="text-h5">Dinner</h5>
+      </v-col>
+      <v-col cols="12">
+        <h5 class="text-h5">Specials</h5>
       </v-col>
     </v-row>
   </div>
