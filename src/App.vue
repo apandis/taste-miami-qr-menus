478b064442017de7a5eaaa01d28b7e6314e1afd6
Antonios Pandis <pandis.a@gmail.com>
2022-03-14 22:53:00 -0400

    add labels

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <v-app>
-  <v-main>
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
-  </v-main>
-</v-app>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
 
-  data () {
+  data() {
     return {
       //
-    }
+    };
   },
-})
+});
 </script>
--- src/views/EpicureView.vue
+++ src/views/EpicureView.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="epicure">
     <v-row>
-      <v-col>
+      <v-col cols="12">
         <h2 class="text-h4">Epicure</h2>
+      </v-col>
+      <v-col cols="12 d-flex justify-space-between">
+        <v-chip label>Breakfast</v-chip><v-chip label>Lunch</v-chip
+        ><v-chip label>Dinner</v-chip>
       </v-col>
     </v-row>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
