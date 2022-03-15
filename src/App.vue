d7ecdf20c6a8942e8a45a401abea0de04f2f16b1
Antonios Pandis <pandis.a@gmail.com>
2022-03-14 21:57:42 -0400

    marenas first test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <!-- <v-navigation-drawer app> -->
+    <!-- -->
+  <!-- </v-navigation-drawer> -->
+
+  <v-app-bar app>
+    <v-app-bar-title>Marenas Beach Resort</v-app-bar-title>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
 <script lang="ts">
---
+++ src/views/MarenasView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="marenas">
+    <h1>Marenas</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
